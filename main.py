6480ee7b80cc4cfb64b816996b4cf468459c403c
joao-vitor-de-souza <joaovitorgoulartsousa@hotmail.com>
2024-06-06 11:12:05 -0300

    11:11_06-06-2024

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ app.include_router(api_router, prefix=settings.AP1_V1_STR)
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run("main:app", host='0.0.0.0', port=8000, log_level='info', reload=True)
+
+
+# http://localhost:8080/phpmyadmin/
